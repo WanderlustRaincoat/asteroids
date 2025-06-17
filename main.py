e7609a992b2d20d3217325971e851eb12047ff32
Nick Mulder <nick.mulder@kiewit.com>
2025-06-17 13:24:35 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+
+
+if __name__ == "__main__":
+    main()
