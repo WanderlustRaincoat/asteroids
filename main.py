4fb999438353b664cfbb8d535546682539e39dea
Nick Mulder <nick.mulder@kiewit.com>
2025-06-20 10:13:29 -0500

    added drawing of player triangle to screen

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from constants import *
+from player import Player 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -13,7 +18,11 @@ def main():
                 return 
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+    
+        #sets the game to 60fps
+        dt = clock.tick(60) / 1000
+    
 if __name__ == "__main__":
     main()
