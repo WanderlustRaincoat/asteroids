3080f253deeed21e855f169f623c2ebde8679309
Nick Mulder <nick.mulder@kiewit.com>
2025-06-24 12:25:02 -0500

    adding in asteroids

--- main.py
+++ main.py
@@ -2,24 +2,39 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from constants import *
-from player import Player 
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
-
+        
+        updatable.update(dt)
+       
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+       
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
     
         #sets the game to 60fps
